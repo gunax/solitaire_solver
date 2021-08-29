071ad693cb10ac3bceecd6ef1ba937d07f32c6dc
jhunter <socrates17@gmail.com>
2021-08-29 01:46:50 -0500

    Added deck-to-board moves

--- src/main/java/solitaire/Match.java
+++ src/main/java/solitaire/Match.java
@@ -1,14 +1,53 @@
 package solitaire;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Match {
     private Top top;
     private Board board;
     private Deck deck;
+    private int moves;
 
-    public Match(Top top, Board board, Deck deck) {
+    public Match(Top top, Board board, Deck deck, int moves) {
         this.top = top;
         this.board = board;
         this.deck = deck;
+        this.moves = moves;
+    }
+
+    public List<Match> moves() {
+        ArrayList<Match> moves = new ArrayList<>();
+
+        moves.addAll(deckToBoardMoves());
+
+        return moves;
+    }
+
+    private List<Match> deckToBoardMoves() {
+        ArrayList<Match> moves = new ArrayList<Match>();
+
+        //try moving top of deck to board
+        if (deck.getTop() != null) {
+            boolean emptyColumnMovedTo = false;
+            for (int i = 0; i < 10; i ++) {
+                Card[] col = board.getColumns()[i];
+                if (col.length == 0) {
+                    if (!emptyColumnMovedTo) {
+                        moves.add(new Match(top, new Board(board, deck.getTop(), i), deck.draw(), this.moves + 1));
+                        emptyColumnMovedTo = true;
+                    }
+                }
+                else {
+                    Card bottomCard = col[col.length-1];
+                    if (bottomCard.getSuit() == deck.getTop().getSuit() && bottomCard.getValue() == deck.getTop().getValue() + 1) {
+                        moves.add(new Match(top, new Board(board, deck.getTop(), i), deck.draw(), this.moves + 1));
+                    }
+                }
+
+            }
+        }
+        return moves;
     }
 
     @Override
--- src/test/java/solitaire/MatchTest.java
+++ src/test/java/solitaire/MatchTest.java
@@ -3,8 +3,9 @@ package solitaire;
 import org.junit.Before;
 import org.junit.Test;
 
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertNotEquals;
+import java.util.List;
+
+import static org.junit.Assert.*;
 
 public class MatchTest {
 
@@ -24,7 +25,7 @@ public class MatchTest {
         card6 = new Card(Card.Suit.Spades, 6);
 
         top = new Top(new int[]{1,2,3,4,5,6,7,8});
-        deck = new Deck(new Card[]{card1, card2, card3, card1, card3}, 2);
+        deck = new Deck(new Card[]{card1, card2, card3, card4, card5}, 2);
 
         Card[][] columns = new Card[10][];
         columns[0] = new Card[]{card1, card2};
@@ -34,13 +35,13 @@ public class MatchTest {
         }
         board = new Board(columns);
 
-        match = new Match(top, board, deck);
+        match = new Match(top, board, deck, 0);
     }
 
     @Test
     public void equals_Test() {
         Top top2 = new Top(new int[]{1,2,3,4,5,6,7,8});
-        Deck deck2 = new Deck(new Card[]{card1, card2, card3, card1, card3}, 2);
+        Deck deck2 = new Deck(new Card[]{card1, card2, card3, card4, card5}, 2);
 
         Card[][] columns = new Card[10][];
         columns[0] = new Card[]{card1, card2};
@@ -49,7 +50,7 @@ public class MatchTest {
             columns[i] = new Card[0];
         }
         Board board2 = new Board(columns);
-        Match match2 = new Match(top2, board2, deck2);
+        Match match2 = new Match(top2, board2, deck2, 1);
 
         assertEquals(match, match2);
     }
@@ -66,7 +67,7 @@ public class MatchTest {
             columns[i] = new Card[0];
         }
         Board board2 = new Board(columns);
-        Match match2 = new Match(top2, board2, deck2);
+        Match match2 = new Match(top2, board2, deck2, 1);
 
         assertNotEquals(match, match2);
     }
@@ -74,7 +75,7 @@ public class MatchTest {
     @Test
     public void differentBoardNotEqual_Test() {
         Top top2 = new Top(new int[]{1,2,3,4,5,6,7,8});
-        Deck deck2 = new Deck(new Card[]{card1, card2, card3, card1, card3}, 2);
+        Deck deck2 = new Deck(new Card[]{card1, card2, card3, card4, card5}, 2);
 
         Card[][] columns = new Card[10][];
         columns[0] = new Card[]{card1, card5};
@@ -83,7 +84,7 @@ public class MatchTest {
             columns[i] = new Card[0];
         }
         Board board2 = new Board(columns);
-        Match match2 = new Match(top2, board2, deck2);
+        Match match2 = new Match(top2, board2, deck2, 1);
 
         assertNotEquals(match, match2);
     }
@@ -91,7 +92,7 @@ public class MatchTest {
     @Test
     public void differentDeckNotEqual_Test() {
         Top top2 = new Top(new int[]{1,2,3,4,5,6,7,8});
-        Deck deck2 = new Deck(new Card[]{card1, card3, card2, card1, card3}, 2);
+        Deck deck2 = new Deck(new Card[]{card1, card3, card2, card4, card5}, 2);
 
         Card[][] columns = new Card[10][];
         columns[0] = new Card[]{card1, card2};
@@ -100,7 +101,7 @@ public class MatchTest {
             columns[i] = new Card[0];
         }
         Board board2 = new Board(columns);
-        Match match2 = new Match(top2, board2, deck2);
+        Match match2 = new Match(top2, board2, deck2, 0);
 
         assertNotEquals(match, match2);
     }
@@ -108,7 +109,7 @@ public class MatchTest {
     @Test
     public void hashEquals_Test() {
         Top top2 = new Top(new int[]{1,2,3,4,5,6,7,8});
-        Deck deck2 = new Deck(new Card[]{card1, card2, card3, card1, card3}, 2);
+        Deck deck2 = new Deck(new Card[]{card1, card2, card3, card4, card5}, 2);
 
         Card[][] columns = new Card[10][];
         columns[0] = new Card[]{card1, card2};
@@ -117,8 +118,31 @@ public class MatchTest {
             columns[i] = new Card[0];
         }
         Board board2 = new Board(columns);
-        Match match2 = new Match(top2, board2, deck2);
+        Match match2 = new Match(top2, board2, deck2, 0);
 
         assertEquals(match.hashCode(), match2.hashCode());
+    }
+
+    @Test
+    public void childrenMoveDeckToEmptyBoardSlot_Test() {
+        Board goalBoard = new Board(board, card3, 0);
+        Deck goalDeck = deck.draw();
+        Match goalMatch = new Match(top, goalBoard, goalDeck, 1);
+        List<Match> children = match.moves();
+
+        assertTrue(children.contains(goalMatch));
+    }
+
+    @Test
+    public void childrenMoveDeckToCompatibleBoardSlot_Test() {
+        Deck startDeck = new Deck(new Card[]{card1, card2, card3, card4, new Card(Card.Suit.Spades, 3)}, 4);
+        Match startMatch = new Match(top, board, startDeck, 1);
+
+        Board goalBoard = new Board(board, new Card(Card.Suit.Spades, 3), 9);
+        Deck goalDeck = new Deck(new Card[]{card1, card2, card3, card4}, 3);
+        Match goalMatch = new Match(top, goalBoard, goalDeck, 1);
+        List<Match> children = startMatch.moves();
+
+        assertTrue(children.contains(goalMatch));
     }
 }
